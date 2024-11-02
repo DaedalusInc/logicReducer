d4e4808f03ecc9090d99970a862faa4084c1199e
Kyle Banister <kyle@thebanisters.org>
2024-11-02 16:57:45 -0700

    Developed input reading system.

--- logicReadIn.c
+++ logicReadIn.c
@@ -1,3 +1,12 @@
+/*Process:
+  Regex for c does not have a way to return the location of the found delimiter, at least that I can find.
+  As such, after turning the entire inputted char array to uppercase, strstr is used to identify the location of an operator.
+  Then, strncpy is used to copy the two halves of the string separated by the delimiter.
+  
+  Code is not yet tested, there are a few spots where I am pretty certain there are errors.*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
@@ -6,38 +15,62 @@
 #define OPCOUNT 3
 
 enum operator {
+    NONE = -1,
     OR,
-    ADD,
-    NOT,
-    SUBTREE
+    AND,
+    NOT
 };
 
 typedef struct treeNode {
-    enum operator;
+    enum operator nodeType;
+    char* contents;
     treeNode* leftChild;
     treeNode* rightChild;
 } treeNode;
 
-void addNode(int opType) {
-    //(treeNode*)malloc(sizeof(treeNode*));
+void setBlankNode(treeNode* inputNode) {
+    inputNode->nodeType = NONE;
+    inputNode->leftChild = NULL;
+    inputNode->rightChild = NULL;
 }
 
- treeMake(char* inputString) {
-    treeNode* rootNode = NULL;
+treeNode* treeMake(char* inputString) {
+    treeNode rootNode = {NONE, strupr(inputString), NULL, NULL};
+    processString(&rootNode);
+}
+
+void processString(treeNode* inputNode) {
+    char* opPtr;
+    int beginOp = 0;
+    int endOp = 0;
+    char* operators[OPCOUNT] = {" OR ", " AND ", "NOT "}; 
     int i = 0;
-    char* operators[OPCOUNT] = {".{0,1} or .{0,1}", ".{0,1} and .{0,1}", ".{0,1}not .{0,1}"}; 
-
-    regex_t regexIn;
-
     
-    for(i = 0; i < OPCOUNT; ++i) {                                                                  //Finds root node
-        regcomp(&regexIn, operators[i], REG_ICASE);
-        if (!regexec(&regexIn, inputString, 0, NULL, 0)) {
-            addNode(i);
-            break;
+    for(i = 0; i < OPCOUNT; ++i) {
+        opPtr = strstr(inputNode->contents, operators[i]);
+        beginOp = strlen(inputNode->contents)-strlen(opPtr);
+        endOp = beginOp + strlen(operators[i]);
+
+        if((i == OR || i == AND) && !strlen(opPtr)) {
+            inputNode->nodeType = i;
+
+            inputNode->leftChild = (treeNode*)malloc(sizeof(treeNode*));
+            strncpy(inputNode->leftChild->contents, inputNode->contents, beginOp);
+            setBlankNode(inputNode->leftChild);
+            processString(inputNode->leftChild);
+
+            inputNode->rightChild = (treeNode*)malloc(sizeof(treeNode*));
+            strncpy(inputNode->rightChild->contents, opPtr, strlen(inputNode->contents)-endOp);
+            setBlankNode(inputNode->rightChild);
+            processString(inputNode->rightChild);
+
+        } else if(i == NOT) {
+            inputNode->nodeType = i;
+
+            inputNode->rightChild = (treeNode*)malloc(sizeof(treeNode*));
+            strncpy(inputNode->rightChild->contents, inputNode->contents, beginOp);
+            processString(inputNode->rightChild);
+            setBlankNode(inputNode->rightChild);
         }
     }
-    
-
-    
 }
